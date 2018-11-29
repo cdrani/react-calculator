d88896b44c58fa9f49108d579b38f2fce96121b8
cdrainxv <cdrainxv@users.noreply.github.com>
2018-11-29 12:28:51 -0700

    style display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const Display = ({ result }) => <h1>{result}</h1>
+const Input = styled.input`
+  display: flex;
+  width: 700px;
+  height: 100px;
+  color: #fff;
+  font-weight: bold;
+  padding: 10px;
+  text-align: right;
+  background-color: #ccc;
+  font-size: 3rem;
+`
+
+const Display = ({ result }) => <Input defaultValue={result} />
 
 Display.propTypes = {
   result: PropTypes.string.isRequired
